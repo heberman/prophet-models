bde3334e88cbda4da6a35c47afd8cb399e5682c4
Henry Berman <heberman@calpoly.edu>
2023-04-18 12:09:53 -0700

    Logvals attempt 1

--- logvals.py
+++ logvals.py
@@ -1,8 +1,13 @@
 import requests
 import time
+import datetime
 
 def logvals():
     while True:
-        res = requests.get('https://thankful-elk-windbreaker.cyclic.app/test')
-        time.sleep(60)
+        now = datetime.datetime.now()
+
+        while now.weekday() in range(1,6) and now.hour >= 5 and now.hour <= 20:
+            res = requests.post('https://thankful-elk-windbreaker.cyclic.app/logvals')
+            time.sleep(3600)
+            now = datetime.datetime.now()
         
